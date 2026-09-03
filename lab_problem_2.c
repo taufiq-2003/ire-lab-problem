0a444d7a77eb4d08f21fe5a5e574b0fb35e2716f
Md. Taufiq Hasan <toufiqhasan2003@gmail.com>
2026-09-03 22:35:11 +0600

    problem 2 complete

--- lab_problem_2.c
+++ lab_problem_2.c
@@ -4,11 +4,11 @@
 int main()
 { 
     
-  int usage_unit,customer_type,cost_per_unit,total;
+  int usage_unit,customer_type,cost_per_unit,total_cost;
 
   float discount,bill;
 
-  printf("enter the amount of units you have used in units: \n");
+  printf("\n\nenter the amount of units you have used in units: \n");
   scanf(" %d",&usage_unit);
 
   printf("\n\nplease select what type of customer are you? \nplease select between 1 to 3  \n");
@@ -19,15 +19,83 @@ int main()
   switch (customer_type)
   {
   case 1:
-    /* residential  */
+    if (usage_unit<100)
+    {
+      total_cost = usage_unit*5;
+    }
+    else if (usage_unit<300)
+    {
+      total_cost= 100*5 + (usage_unit-100)*7;
+    }
+    else {
+      total_cost= 100*5 + 200*7 + (usage_unit-300)*10;
+    }
+
+
+    if (total_cost>2000)
+    {
+      printf("your usage %d   \n",total_cost);
+      
+      printf("you will get 10 percent discount for using over 2000 tk  \n");
+      bill =total_cost- total_cost*0.1;
+      printf("the Bill: %.2f  \n",bill);
+      
+    }
+    else
+    {
+     printf("your usage %d   \n",total_cost);
+      
+      
+      bill =total_cost;
+      printf("the Bill: %.2f  \n",bill);
+    }
+    
+    
+    
     break;
   
   case 2:
-    /* commercial  */
+  if (usage_unit<100)
+    {
+      total_cost = usage_unit*5;
+    }
+    else if (usage_unit<300)
+    {
+      total_cost= 100*5 + (usage_unit-100)*7;
+    }
+    else {
+      total_cost= 100*5 + 200*7 + (usage_unit-300)*10;
+    }
+
+
+    printf("\nyour usage %d   \n",total_cost);
+      
+      printf("as a commercial user wou will receive 5 percent surcharge.  \n");
+      bill =total_cost+ total_cost*0.05;
+      printf("your electricity Bill: %.2f  \n",bill);
+
+
     break;
   
   case 3:
-    /* industrial  */
+    if (usage_unit<100)
+    {
+      total_cost = usage_unit*5;
+    }
+    else if (usage_unit<300)
+    {
+      total_cost= 100*5 + (usage_unit-100)*7;
+    }
+    else {
+      total_cost= 100*5 + 200*7 + (usage_unit-300)*10;
+    }
+
+
+    printf("\nyour usage %d   \n",total_cost);
+      
+      printf("as a industrial user wou will receive 5 percent surcharge.  \n");
+      bill =total_cost+ total_cost*0.1;
+      printf("your electricity Bill: %.2f  \n",bill);
     break;
   
   default:printf("sorry you didnt choose between 1 to 3.please enter valid input  \n");
