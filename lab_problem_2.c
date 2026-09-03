1ae3ce222f2bfb4faf2cc871b9dab5d074d3b365
Md. Taufiq Hasan <toufiqhasan2003@gmail.com>
2026-09-03 22:06:37 +0600

    update basefor problem 2

--- lab_problem_2.c
+++ lab_problem_2.c
@@ -1,10 +1,45 @@
-// Electricity Bill Calculator: Write a C program that takes the number of electricity units consumed and a customer type as input, where customer type is represented by 1 for Residential, 2 for Commercial, and 3 for Industrial. The program should calculate the electricity bill using different rates based on the units consumed: for the first 100 units, the rate is 5 per unit; for the next 200 units, the rate is 7 per unit; and for units above 300, the rate is 10 per unit. After calculating the basic bill, apply a discount of 10% for Residential customers if the basic bill is greater than 2000, while Commercial customers receive a 5% surcharge and Industrial customers receive a 10% surcharge. Use switch to determine the customer type and if, else if, and else to calculate the appropriate bill.
 
 #include <stdio.h> 
 
 int main()
 { 
     
+  int usage_unit,customer_type,cost_per_unit,total;
+
+  float discount,bill;
+
+  printf("enter the amount of units you have used in units: \n");
+  scanf(" %d",&usage_unit);
+
+  printf("\n\nplease select what type of customer are you? \nplease select between 1 to 3  \n");
+  printf("\n1.residential user \n2.commercial user \n3.industrial user  \n");
+
+  scanf("%d", &customer_type);
+
+  switch (customer_type)
+  {
+  case 1:
+    /* residential  */
+    break;
+  
+  case 2:
+    /* commercial  */
+    break;
+  
+  case 3:
+    /* industrial  */
+    break;
+  
+  default:printf("sorry you didnt choose between 1 to 3.please enter valid input  \n");
+  
+    break;
+  }
+  
+
+  
+  
+  
+  
  
 return 0;
 }
